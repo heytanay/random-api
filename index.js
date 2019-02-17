2b559ea21736fa47e3832688a7c9b1e8cf60a6ae
Tanay Mehta <tanaymehta28@gmail.com>
2019-02-17 22:15:03 +0530

    Added Payload Extraction and Better Triming Mechanism:

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const http = require("http");
 const url = require("url");
-
+const StringDecoder = require("string_decoder").StringDecoder;
 
 var server = http.createServer(function(req,res){
     // Get the URL
@@ -12,8 +12,8 @@ var server = http.createServer(function(req,res){
     // Get the Query String
     var queryString = parsedUrl.query;
 
-    // Trim it to Some extent
-    var trimmedPath = path.replace('/','');
+    // Trim the Path Variable
+    var trimmedPath = path.replace(/^\/+|\/+$/g,'');
 
     // Get the Method of Request from the URL and store it's Lower Cased Version into a 'method' object
     var method = req.method.toLowerCase();
@@ -21,14 +21,25 @@ var server = http.createServer(function(req,res){
     // Get the Headers
     var headers = req.headers;
 
-    // End the Process by showing some message 
-    res.end("Welcome to My API testing!");
+    // Get Payload (No Problem if it doesn't exist)
+    var decoder = new StringDecoder('utf-8');
+    var buffer = '';
+    req.on('data',function(data){
+        buffer += decoder.write(data);
+    });
 
-    // Clog out the Trimmed Path, Method, Query String and headers
-    console.log("Request Recieved on "+trimmedPath+" with method "+method+" with the queries: ",queryString);
-    console.log("The Headers are: ",headers);
+    req.on('end',function(){
+        buffer += decoder.end();
+        
+        // End the Process by showing some message 
+        res.end("Welcome to My API testing!");
+        
+        // Clog out the Trimmed Path, Method, Query String, Header and Payload
+        console.log("Request Recieved on "+trimmedPath+" with method "+method+" with the queries: ",queryString);
+        console.log("The Headers are: ",headers);
+        console.log("The Payload is: ",buffer);
+    });
 });
-
 
 server.listen(3000,function(){
     console.log("Server listening on Port: 3000");
